87eb82ad100d55658944eb1968c9dfaf2f7c58b1
Danihr1314 <danihr1314@gmail.com>
2022-10-05 00:03:19 -0500

    Generador maqueteado

---
+++ app.js
@@ -0,0 +1,10 @@
+let range = document.querySelector("[type='range']");
+let text = document.querySelector("[type='text']");
+let p = document.querySelector("p");
+let element = document.querySelector('.square');
+let styleCode = document.querySelector('.css-code');
+
+
+p.addEventListener("change", (e) => {
+  
+})
